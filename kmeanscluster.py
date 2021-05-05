0cbaa246ded07a95d7e0bab458e7f092dc4c45a4
Yeswin Chinta <38446605+yeswinch@users.noreply.github.com>
2021-05-05 03:24:41 -0400

    Add files via upload

---
+++ kmeanscluster.py
@@ -0,0 +1,220 @@
+#!/usr/bin/python
+import time
+import numpy as np
+import random
+from pprint import pprint
+import cv2
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import math
+from statistics import mode
+import time
+
+#locate cluster infor of pixel
+@dataclass
+class clu():
+    r:int
+    g:int
+    b:int
+    cluster:int
+
+
+#calculate euclidean distance
+def euclidDist(a , b):
+    dist = np.linalg.norm(a-b)
+    return(dist)
+
+#format image as [r,g,b,cluster]
+def cluArr(imgarr):
+    cluArray = np.empty((len(imgarr), len(imgarr[0])), dtype=object)
+    for i in range (len(imgarr)):
+        for j in range (len(imgarr[0])):
+            temp= clu(imgarr[i][j][0],imgarr[i][j][1], imgarr[i][j][2], -1)
+            cluArray[i][j] = temp
+    return (cluArray)
+
+#kmeans algo to find centroids of image data
+def kmeans(imgarr):
+    centroids=[]
+    #generate 5 random points
+    for i in range(5):
+        random_y = random.randint(0, len(imgarr) - 1)
+        random_x = random.randint(0, len(imgarr[0]) - 1)
+        centroids.append(list(imgarr[random_y][random_x]))
+
+    ctr=0
+
+    cluArray= cluArr(imgarr)
+    while (ctr!=15):
+        sumarr=[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+        counter=[0,0,0,0,0]
+        ctr=0
+
+        # goes through every pixel
+        for y in range (len(imgarr)):
+            for x in range (len(imgarr[0])):
+                #print("x", x, "y", y)
+                shortestDist=3000
+                cluster=-1
+
+                for j in range(5):
+                    # finding the closest centroid
+                    newDist = euclidDist(imgarr[y][x], centroids[j])
+                    if newDist < shortestDist:
+                        shortestDist=newDist
+                        cluster=j
+
+                #tag the pixel
+                cluArray[y][x].cluster= cluster
+                #add to the array of sums
+                sumarr[cluster][0] +=cluArray[y][x].r
+                sumarr[cluster][1] +=cluArray[y][x].g
+                sumarr[cluster][2] +=cluArray[y][x].b
+                counter[cluster]+= 1
+        #finds the average
+        for k in range(5):
+            for l in range (3):
+                if(counter[k]!= 0):
+                    avg= int(sumarr[k][l]/counter[k])
+                else:
+                    avg= 0
+                if abs(avg-centroids[k][l]) > 5:
+                    centroids[k][l] = avg
+                else:
+                    ctr += 1
+
+
+    return centroids, cluArray
+
+#recolor the left image in terms of representative colors
+def recolorLeft(left, centroids, cluArray):
+    for y in range(0, len(left)):
+        for x in range(0, len(left[0])):
+            left[y][x] = centroids[cluArray[y][x].cluster]
+
+    return left
+
+#a method to turn an image to greyscale
+def toGrey(image):
+    #recolor each pixel
+    copy = np.copy(image)
+    image = image.tolist()
+    for i in range(0,len(image)):
+        for j in range(0, len(image[i])):
+            copy[i][j] = 0.21*image[i][j][0] + 0.72*image[i][j][1] + 0.07*image[i][j][2]
+            image[i][j] = 0.21*image[i][j][0] + 0.72*image[i][j][1] + 0.07*image[i][j][2]
+
+    return np.array(image), copy
+
+#get all of the 3x3 patches in an image
+def get_patches(img):
+    patches=[]
+    #iterate through grayleft
+    #iterate through rows
+    for i in range(1,len(img)-1):
+        #iterate through columns
+        for j in range(1,len(img[0])-1):
+            #grayleft[i][j] starts on middle pixel
+            #find the rest of the patch (adjacent pixels)
+            patches.append((img[i-1:i+2,j-1:j+2],(i,j)))
+
+    return patches
+
+#recolor the right side by finding 6 most similar in testing data
+def recolor_right(right,left):
+    centroids, cluArray = kmeans(left)
+    #FINAL OUTPUT FOR LEFT (representative colors)
+    final_left = np.copy(recolorLeft(left, centroids, cluArray))
+
+    grayleft = toGrey(left)[0]
+
+    grayright, copy= toGrey(right)
+
+    #plt.imshow(grayleft)
+    #plt.show()
+
+    grayleftPatch=get_patches(grayleft)
+
+    tracker = 0
+
+    #iterate through testing
+    #iterate through rows
+    for i in range(1,len(grayright)-1):
+        #iterate through columns
+        for j in range(1,len(grayright[0])-1):
+            patch=grayright[i-1:i+2,j-1:j+2]
+            min1,min2,min3,min4,min5,min6=1000,1000,1000,1000,1000,1000
+            sixPatches=[[],[], [], [], [], []]
+            #find six patches
+
+            #take a sample from the total training data to compare with test data
+            #the higher the number the better the resulting image quality
+            samples = random.sample(list(grayleftPatch), 1000)
+
+            for k in samples:
+                dist=euclidDist(k[0],grayright[i-1:i+2,j-1:j+2])
+                if dist<min1:
+                    min1=dist
+                    sixPatches[1]=sixPatches[0]
+                    sixPatches[0]=k[1]
+                    continue
+                if dist<min2:
+                    min2=dist
+                    sixPatches[2]=sixPatches[1]
+                    sixPatches[1]=k[1]
+                    continue
+                if dist<min3:
+                    min3=dist
+                    sixPatches[3]=sixPatches[2]
+                    sixPatches[2]=k[1]
+                    continue
+                if dist<min4:
+                    min4=dist
+                    sixPatches[4]=sixPatches[3]
+                    sixPatches[3]=k[1]
+                    continue
+                if dist<min5:
+                    min5=dist
+                    sixPatches[5]=sixPatches[4]
+                    sixPatches[4]=k[1]
+                    continue
+                if dist<min6:
+                    min6=dist
+                    sixPatches[5]=k[1]
+                    continue
+
+                #get color of 6 middel pixels
+            for l in range(0,len(sixPatches)):
+                x=sixPatches[l][1]
+                y=sixPatches[l][0]
+
+                #replace the patches/coordinates we got with the colors they represent
+                sixPatches[l] = cluArray[y][x].cluster
+
+            try:
+                mostFrequent=mode(sixPatches)
+                copy[i][j]=centroids[mostFrequent]
+            except:
+                x=random.randint(0,len(sixPatches)-1)
+                tie=sixPatches[x]
+                copy[i][j]=centroids[tie]
+
+            tracker += 1
+        print(tracker/(len(grayright)*len(grayright[0]))*100, "%")
+
+    plt.imshow(final_left)
+    plt.show()
+
+    plt.imshow(copy)
+    plt.show()
+    return final_left, copy
+
+#combine two pictures into one
+def combinePic(final_left, copy):
+    new = []
+    for i in range(0, len(final_left)):
+        new.append(list(final_left[i])+list(copy[i]))
+
+    plt.imshow(new)
+    plt.show()
+    return new
---
+++ neuralnetwork.py
@@ -0,0 +1,124 @@
+import numpy as np
+from kmeanscluster import *
+
+#initialize 2 weight matrices that are randomly generated
+def random_weights():
+	weight1 = np.random.rand(5,9)
+	weight2 = np.random.rand(3,5)
+	return weight1, weight2
+
+#calculate sigmoid function of each value in an array
+def sigmoid(arr):
+	result = 1/(1 + np.exp(-arr))
+	return result
+
+#calculate derivative of sigmoid function of each value in an array
+def sigmoid_prime(arr):
+	result = np.exp(-arr)/np.square(1 + np.exp(-arr))
+	return result
+
+#input_layer is 1D array
+def calc_layers(weight1, weight2, input_layer):
+	hidden_layer = sigmoid(np.dot(weight1,np.transpose(input_layer)))
+	output_layer = sigmoid(np.dot(weight2,hidden_layer))
+	return hidden_layer,output_layer
+
+
+#calculate the cost
+def calc_cost(output_layer, y_layer):
+	sum = 0
+	for a in range(len(output_layer)):
+		sum += np.square(output_layer[a] - y_layer[a])
+	return sum
+
+#calculate the derivative of cost between output + hidden layers
+#should get output of [3x5]
+def calc_cost_deriv_1(hidden_layer, output_layer, y_layer, weight2):
+	term1 = sigmoid_prime(np.dot(weight2,hidden_layer)) #calculate da/dw
+	term2 = 2*np.diagflat(output_layer - np.transpose(np.array([y_layer])))#diagonalize the dC/da term
+	product = np.dot(term2, term1)
+	hidden_layer = np.transpose(hidden_layer) #this is a 1x5 [a,b,c,d,e]
+	adj1 = np.dot(product,hidden_layer) #this is a 3x5 - adjusts weight2 array
+
+	# print("ADJ1",adj1)
+	return adj1
+
+
+
+#calculate the derivative of cost between hidden + input layers
+#should get output of [5x9]
+#dC/dw = dC/da(output) * da(output)/da(hidden) * da(hidden)/dw
+def calc_cost_deriv_2(input_layer, hidden_layer, output_layer, y_layer, weight2, weight1):
+	term1 = sigmoid_prime(np.dot(weight1,np.transpose(input_layer))) #da/dw term: this makes a 5x1 matrix
+	product = np.dot(term1, input_layer) #da/dw w chain rule: this makes a 5x9
+
+	#create a 5x1 matrix: dC/da(output) * da(output)/da(hidden)
+	sum_term = np.array([0.0,0.0,0.0,0.0,0.0])
+	for j in range(3):
+		scalar = 2 * (output_layer[j] - y_layer[j]) * sigmoid_prime(np.dot(weight2[j],hidden_layer))
+		sum_term += (scalar * weight2[j])
+
+	sum_term = np.diagflat(sum_term) #make sum a 5x5 diagonal matrix
+	adj2 = np.dot(sum_term,product) #this is a 5x9 adjustment array - adjusts weight1
+
+	# print("ADJ2", adj2)
+	return adj2
+
+
+
+#take in one patch of 3x3 input data - forward and back propagates
+def train_model(input_data,y_layer, weight1, weight2):
+	#covert to 1x9
+	y_layer=y_layer/255
+	input_data=np.array([input_data.flatten()])/255
+	#forward calculation
+	hidden_layer,output_layer=calc_layers(weight1,weight2,input_data)
+	#find cost
+	cost = calc_cost(output_layer,y_layer)
+	#easy weight derivatives(closest to output)
+	weight2_derivs = calc_cost_deriv_1(hidden_layer, output_layer, y_layer, weight2)
+	#calcuate weight derivatives between input and hidden layers
+	weight1_derivs = calc_cost_deriv_2(input_data, hidden_layer, output_layer, y_layer, weight2, weight1)
+
+	return weight2_derivs, weight1_derivs
+
+
+
+#trains the model on an image
+def training_data(left):
+	leftcopy = np.copy(left)
+	greyleft = toGrey(left)[0] #turns left image into greyscale
+	patches = get_patches(greyleft) #gets all patches from left image
+	np.random.shuffle(patches)
+	weight1, weight2 = random_weights()
+
+	#patches in format [[a,b,c],[d,e,f],[g,h,i],(i,j)]
+	group = []
+	numgroups = 200
+	#split into batches
+	for x in range(numgroups):
+		group.append(patches[int(x*len(patches)/numgroups):int(len(patches)/numgroups*(1+x)-1)])
+	for subgroup in group:
+		#look through each batch a certain number of times
+		for j in range(100):
+			adj1_total = np.zeros((3,5),dtype = float)
+			adj2_total = np.zeros((5,9),dtype = float)
+			for patch in subgroup:
+				#train model
+				a1, a2 = train_model(patch[0],leftcopy[patch[1]], weight1, weight2)
+				adj1_total += a1
+				adj2_total += a2
+			adj1_avg = adj1_total / len(subgroup)
+			adj2_avg = adj2_total / len(subgroup)
+			weight1 = weight1 - adj2_avg
+			weight2 = weight2 - adj1_avg
+	return weight1, weight2
+
+#use the trained model to recolor
+def use_model(weight1,weight2,rightgray,rightRGB):
+	right_patches=get_patches(rightgray)
+	for i in right_patches:
+		x=i[0].flatten()/255
+		output=calc_layers(weight1,weight2,x)[1]
+		rightRGB[i[1]]=output*255
+	return rightRGB
